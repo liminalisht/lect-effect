dce5664c243ad14e72767c106e64b7d1b9fd8436
Abraham Howland <ahowland@gmail.com>
2026-01-06 12:45:29 -0600

    iterate

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -2,11 +2,13 @@ import { Schema } from "effect"
 import { query, resolver } from "@gqloom/core"
 import * as schemas from "./schemas"
 
+type NameInput = Schema.Schema.Type<typeof schemas.NameInputSchema>
+
 const helloResolver = resolver({
   hello: query(Schema.standardSchemaV1(schemas.HelloResponseSchema))
     .input(Schema.standardSchemaV1(schemas.NameInputSchema))
     //todo: extract handler, use effect to run, providing services
-    .resolve((input) => `Hello, ${input.name}!`),
+    .resolve((input: NameInput) => `Hello, ${input.name}!`),
 })
 
 export const resolvers = [
