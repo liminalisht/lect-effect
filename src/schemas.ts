e4d89c7dfb4c04a13db2e47acaf11550a39731fa
Abraham Howland <ahowland@gmail.com>
2026-01-06 15:56:03 -0600

    iterate

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,31 +1,35 @@
 import { Arbitrary, Schema } from "effect"
+import { asField } from "@gqloom/effect"
 
-//todo: determine why these descriptions don't show up in GraphQL Playground
+export const NameSchema = Schema.String.annotations({
+  title: "Name",
+  description: "Name to greet",
+})
+export type Name = Schema.Schema.Type<typeof NameSchema>
+export const NameArbitrary = Arbitrary.make(NameSchema)
+
 export const NameInputSchema = Schema.Struct({
-  name: Schema.NullishOr(Schema.String).annotations({
+  name: Schema.NullishOr(NameSchema).annotations({
     description: "Optional name to greet",
   }),
 }).annotations({
+  title: "NameInput",
   description: "Input schema for name",
 })
 export type NameInput = Schema.Schema.Type<typeof NameInputSchema>
 export const NameInputArbitrary = Arbitrary.make(NameInputSchema)
 
 export const HelloResponseSchema = Schema.String.annotations({
+  title: "HelloResponse",
   description: "Response schema for hello",
+  [asField]: {
+    description: "Greets the caller, optionally using the provided name",
+  },
 })
 export type HelloResponse = Schema.Schema.Type<typeof HelloResponseSchema>
 export const HelloResponseArbitrary = Arbitrary.make(HelloResponseSchema)
 
-export const NameInputStandard = Schema.standardSchemaV1(
-  NameInputSchema.annotations({
-    description: "NameInput",
-  }),
-)
+export const NameInputStandard = Schema.standardSchemaV1(NameInputSchema)
 
-export const HelloResponseStandard = Schema.standardSchemaV1(
-  HelloResponseSchema.annotations({
-    description: "HelloResponse",
-  }),
-)
+export const HelloResponseStandard = Schema.standardSchemaV1(HelloResponseSchema)
 
