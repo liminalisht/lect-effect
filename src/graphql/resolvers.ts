de965cc4e796d63ddee88762ddaeabbf94e10bbc
Abraham Howland <ahowland@gmail.com>
2026-01-08 14:11:51 -0600

    iterate

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -3,6 +3,7 @@ import * as handlers from "../domain/handlers"
 import * as schemas from "../domain/schemas"
 import { runEffect } from "../runEffect"
 
+// todo: can't i make this use Effect?
 export const makeResolvers = () => {
   const helloResolver = resolver({
     hello:
--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -4,4 +4,5 @@ import { asyncContextProvider } from "@gqloom/core/context"
 import { makeResolvers } from "./resolvers"
 
 // asyncContextProvider is enabled, so you can just read the GraphQLContext via useContext.
+// todo: can't i make this use Effect?
 export const schema = weave(EffectWeaver, asyncContextProvider, ...makeResolvers())
