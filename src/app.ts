abef3cc448427107653c3ed174aa33074ca0e7e5
Abraham Howland <ahowland@gmail.com>
2026-01-08 15:26:45 -0600

    iterate

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+// todo: where to put this import? it is needed for its side effects
+import 'dotenv/config';
+
+import { Effect } from 'effect';
+import { logSchema, schema } from './graphql/schema';
+import { listen } from './graphql/server';
+import { makeYoga } from './graphql/yoga';
+import { AppEnv, ConfigService } from './services';
+
+// todo: grok Effect.scoped and Effect.gen interaction better
+export const app: Effect.Effect<never, unknown, AppEnv>
+  = Effect.scoped(Effect.gen(function * () {
+    yield * Effect.logDebug('getting config...');
+    const config = yield * ConfigService;
+
+    // todo: maybe have some service that provides the schema? and logging and serving mechanisms?
+    yield * Effect.logDebug('logging graphql schema...');
+    yield * logSchema(schema);
+
+    yield * Effect.logDebug('making yoga server instance...');
+    const yoga = yield * makeYoga;
+
+    yield * Effect.logDebug('starting server listener...');
+    yield * listen(yoga, config.port);
+    yield * Effect.logInfo(`server is running on http://localhost:${config.port}/graphql`);
+
+    return yield * Effect.never;
+  }));
--- src/index.ts
+++ src/index.ts
@@ -6,34 +6,35 @@ import { listen } from './graphql/server';
 import { makeYoga } from './graphql/yoga';
 import { AppLayer } from './layers/app';
 import { AppEnv, ConfigService } from './services';
+import { app } from './app';
 
-// todo: grok Effect.scoped and Effect.gen interaction better
-const program: Effect.Effect<never, unknown, AppEnv>
-  = Effect.scoped(Effect.gen(function * () {
-    yield * Effect.logDebug('getting config...');
-    const config = yield * ConfigService;
+// // todo: grok Effect.scoped and Effect.gen interaction better
+// const program: Effect.Effect<never, unknown, AppEnv>
+//   = Effect.scoped(Effect.gen(function * () {
+//     yield * Effect.logDebug('getting config...');
+//     const config = yield * ConfigService;
 
-    // todo: maybe have some service that provides the schema? and logging and serving mechanisms?
-    yield * Effect.logDebug('logging graphql schema...');
-    yield * logSchema(schema);
+//     // todo: maybe have some service that provides the schema? and logging and serving mechanisms?
+//     yield * Effect.logDebug('logging graphql schema...');
+//     yield * logSchema(schema);
 
-    yield * Effect.logDebug('making yoga server instance...');
-    const yoga = yield * makeYoga;
+//     yield * Effect.logDebug('making yoga server instance...');
+//     const yoga = yield * makeYoga;
 
-    yield * Effect.logDebug('starting server listener...');
-    yield * listen(yoga, config.port);
-    yield * Effect.logInfo(`server is running on http://localhost:${config.port}/graphql`);
+//     yield * Effect.logDebug('starting server listener...');
+//     yield * listen(yoga, config.port);
+//     yield * Effect.logInfo(`server is running on http://localhost:${config.port}/graphql`);
 
-    return yield * Effect.never;
-  }));
+//     return yield * Effect.never;
+//   }));
 
-const programWithAppLayer: Effect.Effect<never, unknown>
-  = program.pipe(Effect.provide(AppLayer));
+// const programWithAppLayer: Effect.Effect<never, unknown>
+//   = program.pipe(Effect.provide(AppLayer));
 
 // todo: i would have thought that there was some way to actually catch and log all unhandled errors globally, rather
 // than using console.log .... i mean clearly we want the error cause to be Effect.logError'ed properly...
 NodeRuntime.runMain(
-  programWithAppLayer,
+  app.pipe(Effect.provide(AppLayer)),
   {
     teardown(exit, onExit) {
       if (exit._tag === 'Failure') {
