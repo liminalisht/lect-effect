4ac56db1c9aff378afd2f81929a2ec7e4ecb0a84
Abraham Howland <ahowland@gmail.com>
2026-01-08 08:50:53 -0600

    iterate

--- src/env.ts
+++
@@ -1,4 +0,0 @@
-export { AppConfigService as AppConfig} from "./services/config"
-export { ConfigLayer as AppConfigLive } from "./layers/config"
-export { LoggerLayer } from "./layers/logger"
-export { AppLayer } from "./layers/app"
