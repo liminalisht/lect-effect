29f729e8ee7763e8f971c4dfe408004899054d66
Abraham Howland <ahowland@gmail.com>
2026-01-08 16:32:17 -0600

    iterate

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,13 @@ import { NodeRuntime } from '@effect/platform-node';
 import { app } from './app';
 import { appLayer } from './layers/app';
 
-// todo: i would have thought that there was some way to actually catch and log all unhandled errors globally, rather
-// than using console.log .... i mean clearly we want the error cause to be Effect.logError'ed properly...
-NodeRuntime.runMain(
-  app.pipe(Effect.provide(appLayer)),
-  {
-    teardown(exit, onExit) {
-      if (exit._tag === 'Failure') {
-        console.error('program ended with an error', exit.cause);
-        onExit(1);
-      } else {
-        onExit(0);
-      }
-    },
-  },
+const main = app.pipe(
+  Effect.provide(appLayer),
+  Effect.onExit(exit =>
+    (exit._tag === 'Failure'
+      ? Effect.logError('application failed with cause:', exit.cause)
+      : Effect.void)),
 );
+
+NodeRuntime.runMain(main);
 
