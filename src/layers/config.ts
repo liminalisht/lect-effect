5f2ec51c1f6df011c770c28da644bc0eadd6f4e5
Abraham Howland <ahowland@gmail.com>
2026-01-08 14:27:25 -0600

    iterate

--- src/layers/config.ts
+++ src/layers/config.ts
@@ -7,8 +7,8 @@ export const ConfigLayer: Layer.Layer<ConfigService, ConfigError.ConfigError>
   = Layer.effect(
     ConfigService,
     Effect.gen(function * () {
-      const port = yield * Config.number('PORT').pipe(Config.withDefault(4000));
+      const port = yield * Config.number('PORT').pipe(Config.withDefault(4000)).pipe(Effect.map(Port));
       const logLevel = yield * Config.logLevel('LOGLEVEL').pipe(Config.withDefault(LogLevel.Info));
-      return { port: Port(port), logLevel };
+      return { port, logLevel };
     }),
   );
