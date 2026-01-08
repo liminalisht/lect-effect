6e8218ae1c46b569b3893f48a83bde85d1e70be2
Abraham Howland <ahowland@gmail.com>
2026-01-08 16:14:10 -0600

    iterate

--- src/domain/schemas.ts
+++ src/domain/schemas.ts
@@ -4,28 +4,28 @@ export type Name = Schema.Schema.Type<typeof nameSchema>;
 
 export const nameSchema = Schema.String;
 
+// todo: move or remove
 export const nameArbitrary = Arbitrary.make(nameSchema);
 
 export type NameInput = Schema.Schema.Type<typeof nameInputSchema>;
 
 export const nameInputSchema = Schema.Struct({
   name: Schema.NullishOr(nameSchema).annotations({
-    description: 'Optional name to greet',
+    description: 'optional name to greet',
   }),
 });
 
+// todo: move or remove
 export const nameInputArbitrary = Arbitrary.make(nameInputSchema);
 
 export type HelloResponse = Schema.Schema.Type<typeof helloResponseSchema>;
 
 export const helloResponseSchema = Schema.Struct({
   greeting: Schema.String.annotations({
-    description: 'Greeting message',
+    description: 'greeting message',
   }),
 }).annotations({
   title: 'HelloResponse',
-  description: 'Response payload for hello',
+  description: 'response for hello',
 });
-
-export const helloResponseArbitrary = Arbitrary.make(helloResponseSchema);
 
--- todo.md
+++ todo.md
@@ -17,3 +17,4 @@
 - [ ] organize tests according to whether they test effects against the graphql boundary or not
 - [ ] create first test that uses arbitraries
 - [ ] create first test that hits graphql boundary, uses arbitraries for input, and makes property assertions about output
+- [ ] i'm wondering whether we should organize our repo in a way that separates domain types, services, and service implementations
