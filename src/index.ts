b8c2951c95736350408c255bd046df43e1081828
Abraham Howland <ahowland@gmail.com>
2026-01-09 09:50:06 -0600

    iterate

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
 import 'dotenv/config';
-import { Effect } from 'effect';
+import { Cause, Effect } from 'effect';
 import { NodeRuntime } from '@effect/platform-node';
 import { app } from './app';
 import { appLayer } from './layers/app';
 
-const main = app.pipe(
+const logFailure = (cause: Cause.Cause<unknown>) =>
+  Effect.all([
+    Effect.logError('application failed'),
+    Effect.logError(Cause.pretty(cause)),
+  ]);
+
+const main: Effect.Effect<never, unknown, never> = app.pipe(
   Effect.provide(appLayer),
   Effect.onExit(exit =>
-    (exit._tag === 'Failure'
-      ? Effect.logError('application failed with cause:', exit.cause)
-      : Effect.void)),
+    exit._tag === 'Failure'
+      ? logFailure(exit.cause)
+      : Effect.void,
+  ),
 );
 
 NodeRuntime.runMain(main);
