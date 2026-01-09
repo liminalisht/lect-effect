cf91e77b43b83516dc362837d272896fa48bcd39
Abraham Howland <ahowland@gmail.com>
2026-01-08 23:49:20 -0600

    iterate

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,6 +1,6 @@
 import { Schema } from 'effect';
 import { query, resolver } from '@gqloom/core';
-import * as schemas from '../domain';
+import * as domain from '../domain';
 import * as handlers from '../handlers';
 import { runEffect } from './effect';
 
@@ -8,8 +8,8 @@ import { runEffect } from './effect';
 export const makeResolvers = () => {
   const helloResolver = resolver({
     hello:
-      query(Schema.standardSchemaV1(schemas.helloResponseSchema))
-        .input(Schema.standardSchemaV1(schemas.nameInputSchema))
+      query(Schema.standardSchemaV1(domain.helloResponseSchema))
+        .input(Schema.standardSchemaV1(domain.nameInputSchema))
         .resolve(async args =>
           runEffect(handlers.helloHandler(args))),
   });
