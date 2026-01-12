d6d0eea77bfc23715764a7d3258e70e6125f744d
Abraham Howland <ahowland@gmail.com>
2026-01-11 22:15:11 -0600

    iterate

--- test/layers/app.ts
+++ test/layers/app.ts
@@ -2,7 +2,6 @@ import { Layer } from 'effect';
 import { greetingLayer } from '../../src/layers/greeting';
 import { testConfigLayer } from './config';
 import { testLoggerLayer } from './logger';
-import { MasterdataDb } from '../../src/services/masterdataDb';
 import { masterdataDbLayer } from '../../src/layers/masterdataDb';
 import { AppError } from '../../src/errors';
 import { AppServices } from '../../src/services';
