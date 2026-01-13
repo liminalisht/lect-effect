6210ae413994c3e7a41af97804fbe0c5b2f32117
Abraham Howland <ahowland@gmail.com>
2026-01-13 15:22:55 -0600

    iterate

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,12 +2,12 @@
 import {defineConfig} from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
+// See https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'lect-effect',
-			social: [{icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight'}],
+			social: [{icon: 'github', label: 'GitHub', href: 'https://github.com/liminalisht/lect-effect'}],
 			sidebar: [
 				{
 					label: 'guides',
