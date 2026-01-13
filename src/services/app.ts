59c21caef6ce636becd12b95af68fd01e6f0ef05
Abraham Howland <ahowland@gmail.com>
2026-01-13 00:34:13 -0600

    iterate

--- src/services/app.ts
+++ src/services/app.ts
@@ -4,10 +4,6 @@ import { type ItemRepo } from './itemRepo';
 import { type MasterdataDb } from './masterdataDb';
 import { type ProductRepo } from './productRepo';
 
-export { ConfigService } from './config';
-export { GreetingService } from './greeting';
-
-// todo: extract to app.ts? or rename index to app?
 export type AppServices =
 	ConfigService
 	| GreetingService
