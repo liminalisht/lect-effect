f7c520130c90eb466eb8b3cfd917a169e9c37a32
Abraham Howland <ahowland@gmail.com>
2026-01-13 00:32:05 -0600

    iterate

--- src/layers/logger.ts
+++ src/layers/logger.ts
@@ -2,7 +2,7 @@ import { Effect, Layer, Logger } from 'effect';
 import { ConfigService } from '../services/config';
 
 export const loggerLayer: Layer.Layer<never, never, ConfigService>
-= Layer.unwrapEffect(Effect.gen(function * () {
-  const { app } = yield * ConfigService;
-  return Logger.minimumLogLevel(app.logLevel);
-}));
+  = Layer.unwrapEffect(Effect.gen(function * () {
+    const { app } = yield * ConfigService;
+    return Logger.minimumLogLevel(app.logLevel);
+  }));
