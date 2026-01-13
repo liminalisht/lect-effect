e00ead6fe284cd78746cbd898be41835af81c436
Abraham Howland <ahowland@gmail.com>
2026-01-13 00:56:24 -0600

    iterate

---
+++ learning.md
@@ -0,0 +1,4 @@
+# learning resources
+
+- [typescript handbook](https://www.typescriptlang.org/docs/handbook/intro.html)
+- [effect](https://effect.website/docs/)
--- src/errors.ts
+++ src/errors.ts
@@ -3,7 +3,7 @@ import { type DomainError } from './domain/errors';
 import { type GraphqlError } from './graphql/errors';
 import { ConfigurationError } from './config/errors';
 
-// todo: this can't be right. it can't be exhaustive
+// todo: this can't be right. it can't be exhaustive... but why would the compiler check then?
 export type AppError =
   ConfigurationError
   | DomainError
