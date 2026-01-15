2c47769598cbf28444184f7f9e0ac81fefa42188
Abraham Howland <ahowland@gmail.com>
2026-01-15 15:31:46 -0600

    iterate

--- src/services/interfaces/config.ts
+++ src/services/interfaces/config.ts
@@ -3,7 +3,7 @@
  * @since 1.0.0
  */
 import { Context } from 'effect';
-import { type AppConfig } from '../../config/app';
+import { type AppConfig } from './config/appConfig/appConfig';
 import { type MasterdataDbConfig } from '../../config/masterdataDb';
 
 /**
