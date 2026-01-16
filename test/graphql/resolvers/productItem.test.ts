e278283804686673999870d53a2dfb5afa166bca
Abraham Howland <ahowland@gmail.com>
2026-01-15 23:26:14 -0600

    iterate

--- test/graphql/resolvers/productItem.test.ts
+++ test/graphql/resolvers/productItem.test.ts
@@ -3,38 +3,27 @@ import * as fc from 'fast-check';
 import {
   Arbitrary,
   Effect,
-  Layer,
-  LogLevel,
-  Option,
-  Redacted,
 } from 'effect';
-import type * as SqlClient from '@effect/sql/SqlClient';
 import { makeSchema, type GraphQLResolver } from '../../../src/graphql/schema';
 import { handlersToResolvers } from '../../../src/graphql/resolvers';
 import { helloHandlers } from '../../../src/handlers/hello';
 import { itemHandlers } from '../../../src/handlers/item';
 import { productHandlers } from '../../../src/handlers/product';
 import { makeYoga, type Yoga } from '../../../src/graphql/yoga';
-import { ConfigService } from '../../../src/services/config/interface';
-import { GreetService } from '../../../src/services/greeting/interface';
 import { MasterdataDbService } from '../../../src/services/masterdataDb/interface';
-import { ProductRepoService, type ProductRepoShape } from '../../../src/services/productRepo/interface';
-import { ItemRepoService, type ItemRepoShape } from '../../../src/services/itemRepo/interface';
-import { type Product } from '../../../src/domain/product/product';
-import { type Item } from '../../../src/domain/item/item';
 import { createProductWithItemsInputSchema, type CreateProductWithItemsInput } from '../../../src/domain/product/createProductWithItemsInput';
-import { type Port } from '../../../src/services/appConfig/interface/port';
-import { type Greeting } from '../../../src/domain/hello/greeting';
 import { type AppServices } from '../../../src/services/app/interface';
 import { testAppLayer } from '../../layers/app';
 
+// todo: extract - maybe even explicitly in app.ts for reuse?
 const schema = makeSchema(handlersToResolvers([
   ...helloHandlers,
   ...itemHandlers,
   ...productHandlers,
 ]) as readonly GraphQLResolver[]);
 
-const arbitraryCreateProductWithItemsInput = Arbitrary.make(createProductWithItemsInputSchema);
+const arbitraryCreateProductWithItemsInput: fc.Arbitrary<CreateProductWithItemsInput>
+  = Arbitrary.make(createProductWithItemsInputSchema);
 const arbitraryCreateProductWithItemsInputNonEmpty: fc.Arbitrary<CreateProductWithItemsInput>
   = arbitraryCreateProductWithItemsInput.filter((input: CreateProductWithItemsInput) => input.items.length > 0);
 
@@ -42,74 +31,6 @@ const normalizeInput = (input: CreateProductWithItemsInput): CreateProductWithIt
   product: { description: input.product.description ?? null },
   items: input.items.map(item => ({ description: item.description ?? null, pack_size: item.pack_size })),
 });
-
-const makeAppLayer = () => {
-  let nextProductId = 1;
-  let nextItemId = 1;
-  const products: Product[] = [];
-  const items: Item[] = [];
-  const links = new Map<Item['id'], Product['id']>();
-
-  const productRepo: ProductRepoShape = {
-    getById: id => Effect.succeed(Option.fromNullable(products.find(p => p.id === id))),
-    getForItem: itemId => Effect.succeed(Option.fromNullable(links.get(itemId)).pipe(Option.flatMap(productId => Option.fromNullable(products.find(p => p.id === productId))))),
-    list: Effect.succeed(products),
-    create(input) {
-      const product: Product = {
-        __typename: 'Product',
-        id: nextProductId as Product['id'],
-        description: input.description ?? null,
-      };
-      nextProductId += 1;
-      products.push(product);
-      return Effect.succeed(product);
-    },
-  };
-
-  const itemRepo: ItemRepoShape = {
-    getById: id => Effect.succeed(items.find(i => i.id === id) ?? null),
-    list: Effect.succeed(items),
-    create(input) {
-      const item: Item = {
-        id: nextItemId as Item['id'],
-        description: input.description ?? null,
-        pack_size: input.pack_size,
-      };
-      nextItemId += 1;
-      items.push(item);
-      return Effect.succeed(item);
-    },
-    listForProduct: productId => Effect.succeed(items.filter(item => links.get(item.id) === productId)),
-    linkToProduct(itemId, productId) {
-      links.set(itemId, productId);
-      return Effect.succeed(undefined);
-    },
-  };
-
-  const configLayer = Layer.succeed(ConfigService, {
-    app: { port: 3000 as Port, logLevel: LogLevel.Info, environment: 'test' },
-    masterdataPg: {
-      url: Redacted.make('postgres://test'),
-      pool: { min: 0, max: 1, idleTimeoutMillis: 0 },
-    },
-  });
-
-  const greetingLayer = Layer.succeed(GreetService, {
-    greet: name => Effect.succeed(Option.match(name, { onNone: () => 'World', onSome: n => n }) as Greeting),
-  });
-
-  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-  const masterdataLayer = Layer.succeed(MasterdataDbService, { sql: {} as SqlClient.SqlClient });
-
-  const reposLayer = Layer.mergeAll(
-    Layer.succeed(ProductRepoService, productRepo),
-    Layer.succeed(ItemRepoService, itemRepo),
-  );
-
-  const appLayer = Layer.mergeAll(configLayer, greetingLayer, masterdataLayer, reposLayer);
-
-  return { appLayer, state: { products, items, links } } as const;
-};
 
 const fetchJson = async (yoga: Yoga<AppServices>, query: string, variables?: Record<string, unknown>) => {
   const res = await yoga.fetch('http://unused/graphql', {
