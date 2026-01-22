f1f75760575712974f2f0be57117f6c5568b6abc
Abraham Howland <ahowland@gmail.com>
2026-01-22 12:05:42 -0600

    iterate

--- xo.config.ts
+++ xo.config.ts
@@ -59,7 +59,7 @@ const xoConfig: FlatXoConfig = [
     },
   },
   {
-    files: ['test/**', 'packages/domain/test/**'],
+    files: ['apps/backend/test/**', 'packages/domain/test/**'],
     rules: {
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-call': 'off',
